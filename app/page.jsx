856c176448a517e8a36285a21f6e68624bc38371
GuiltyGamingJay <110130238+GuiltyGamingJay@users.noreply.github.com>
2024-03-01 18:31:27 -0500

    Fixed image, working why quiz stopped working

--- app/page.jsx
+++ app/page.jsx
@@ -4,24 +4,36 @@ import Image from 'next/image'
 import Image2 from './Assets/Images/bgojo.jpg';
 import React from 'react';
 import './globals.css';
+import styles from './Home.module.css';
 
 
 
 
-export default function Home() {
+export default 
+
+function Home() {
   return (
-    <main > <div className="container">
-      <Image src="/yugi.png" layout="fill"
-      classname="logo"
+    <main > 
+      
+      <div className="container">
+      
+      <Image className={styles.circularLogo}
+      src="/yugi.png" 
+      layout="fixed"
+      width={300}
+      height={300}
+    alt="Logo"
+      
       />
-      <Image src={logo} />
-     
+      
+      
       
         <h1>Quiz App</h1>
         <Link href='/quiz'>
         <button> Start Quiz</button>
         </Link>
       </div>
+      
     </main>
   );
 }
